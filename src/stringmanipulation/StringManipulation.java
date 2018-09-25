128948a162ee9bfa95518f0a81c274af994b3570
suleyman.yildirim <suleyman.yildirim@CPX-IOTTIBSY5PL.dir.svc.accenture.com>
2018-09-25 22:50:03 +0100

    String immutability and equality

---
+++ src/stringmanipulation/StringManipulation.java
@@ -0,0 +1,39 @@
+package stringmanipulation;
+
+public class StringManipulation {
+
+	public static void main(String[] args) {
+		immutability();
+		equality();
+	}
+
+	private static void equality() {
+		
+		String s1 = "Java"; // Use string pool	
+		String s2 = new String("Java"); // don't use string pool and create object
+		if (s1 == s2) {
+			System.out.println("Object references are equal");
+		}
+		
+		if (s1.equals(s2)) {
+			System.out.println("Contents of the objects are equal");
+		}
+
+	}
+
+	/**
+	 * 
+	 */
+	private static void immutability() {
+		// String is immutable so an object cannot be modified or garbage collected
+		String s = "Java";
+		s.concat("8");
+		System.out.println(s);
+		
+		// StringBuilder can be mutated with methods like append()
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Java");
+		System.out.println(stringBuilder);
+	}
+
+}
